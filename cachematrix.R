43038b84d84930dbe0bf23b119d9bbe34c801e97
Dmytro Kovalchuk <kdmytro@gmail.com>
2015-11-17 22:51:03 -0800

    final version of the programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera - R Programming
+## Programming Assessment 2
+## written by Dmytro Kovalchuk <kdmytro@gmail.com>
+##
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a "cache-able" matrix from its argument x, assigns getters and setters methods 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    s <- NULL
+    
+    set <- function (y){
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    
+    ## saving "value" to cache 's'
+    setSolve <- function(value) s <<- value
+    
+    ## return cached value '"'s'
+    getSolve <- function() s
+    
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function returns a chached inverse of matrix 'x' 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## get cached value
+    s <- x$getSolve();
+    
+    ## return cached value if exists
+    if(!is.null(s)){
+        message("getting cached data")
+        return(s)
+    }
+    
+    ## solve for 's'
+    data <- x$get()
+    s <- solve(data, ...)
+    
+    ## save 's' to cache
+    x$setSolve(s)
+    
+    s
 }
